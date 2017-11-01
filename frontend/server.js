90b4a877dedcc0fa3287c7adae6720fdd5ae63ea
Flatzs <gagelangdon@gmail.com>
2017-11-01 12:03:28 -0700

    change how im serving frontend app

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,8 @@ const Express = require('express');
 const Server = Express();
 const path = require('path');
 
-Server.use(Express.static(path.join(__dirname, './build')));
+Server.get('*', (req, res) => {
+	res.send(Express.static(path.join(__dirname, './build')));
+});
 
 Server.listen('3000', () => console.log('server started on 3000'));
