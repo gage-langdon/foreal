15eeebe6973f00cf31e7210e476de3e7137b8d6b
Flatzs <gagelangdon@gmail.com>
2017-11-01 12:08:55 -0700

    undo

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,5 @@ const Express = require('express');
 const Server = Express();
 const path = require('path');
 
-Server.get('*', (req, res) => {
-	res.send(Express.static(path.join(__dirname, './build')));
-});
-
+Server.use(Express.static(path.join(__dirname, './build')));
 Server.listen('3000', () => console.log('server started on 3000'));
