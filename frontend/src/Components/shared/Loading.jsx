3f15d53e07286906ebd07936708beb6ceae84dca
Flatzs <gagelangdon@gmail.com>
2017-11-02 20:23:23 -0700

    change loading anim color to lighter black

--- frontend/src/Components/shared/Loading.jsx
+++ frontend/src/Components/shared/Loading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Spinner from 'react-spinkit';
 
-export default ({ isLoading=true, color }) => {
-	if (isLoading) return <Spinner name="wave" fadeIn="none" color={color || "#161616"} />;
+export default ({ isLoading = true, color }) => {
+	if (isLoading) return <Spinner name="wave" fadeIn="none" color={color || 'rgb(0,0,0,.35)'} />;
 	else return null;
 };
