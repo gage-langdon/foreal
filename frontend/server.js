591748a2036af7ef7d60a3ac06fa6cd9429ce486
Flatzs <gagelangdon@gmail.com>
2018-01-02 21:27:01 -0800

    feat: add compression

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,11 @@
 const Express = require('express');
+const Compression = require('compression');
 const Server = Express();
 const path = require('path');
 
+Server.use(Compression());
 Server.use(Express.static(path.join(__dirname, './build')));
+
 Server.get('*', (req, res) => {
 	res.sendFile(`${__dirname}/build/index.html`);
 });
